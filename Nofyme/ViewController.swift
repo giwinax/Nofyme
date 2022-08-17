b571526060de14e96982bfb3bf1519c4d6c5c3d3
Stanislav <giwinax@gmail.com>
2022-08-17 14:15:04 +0300

    Initial Commit

---
+++ Nofyme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nofyme
+//
+//  Created by s b on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
